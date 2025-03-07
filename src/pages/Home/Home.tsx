d36ccff53a922650a27e94522f6b4e36866f1429
Sahil Rawat <sahilr8587@gmail.com>
2025-03-07 18:17:32 +0530

    Add Zigzag and Bg components to Home page; update Display component for improved image effects

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import About from '@/components/About'
 import Display from './components/Display'
 import Banner from './components/Banner'
 import Hero from '@/components/Hero'
+import Zigzag from './components/Zigzag'
+import Bg from './components/Bg'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <About />
       <Display />
       <Banner />
+      <Zigzag />
+      <Bg />
     </div>
   )
 }
---
+++ src/pages/Home/components/Zigzag.tsx
@@ -0,0 +1,59 @@
+
+const Zigzag = () => {
+  return (
+    <div  className="bg-[#FCF5EA] px-10 md:px-20 w-full overflow-hidden flex flex-col justify-center items-center">
+    <div className="flex flex-row-reverse justify-between items-center py-0 md:py-20 w-full ">
+    <div >
+        
+        <p className="mt-4 text-sm md:text-2xl text-[#1B1C1E] text-center w-[40vw] md:w-[30vw] ">
+        Our commitment to sustainability and the art of whiskey making extends beyond the bottle
+        </p>
+        
+      </div>
+
+      <div className="relative">
+      
+        <div className="relative ">
+          
+          <img
+            src="/images/moet.png" 
+            alt="MOET"
+            className="  w-[100%] h-[20vh] md:h-[90vh] mr-2 relative z-[2] transform translate-y-4 hover:scale-105 transition-transform duration-500"
+          />
+          
+          
+        </div>
+      </div>
+
+      
+    </div>
+    <div className="flex flex-row justify-center gap-8 items-center">
+    <div className=" flex items-center" >
+        
+        <p className="mt-4 text-xs md:text-lg text-[#1B1C1E] text-center w-[40vw] md:w-[30vw] ">
+        As the amber liquid gracefully swirls in the glass, it tells tales of time-honored traditions, the care of countless craftsmen, and the serene landscapes of its birth        </p>
+        
+      </div>
+
+      <div className="relative">
+      
+        <div className="relative ">
+          
+          <img
+            src="/images/jacob's creek.png" 
+            alt="MOET"
+            className="  h-[20vh] md:h-[70vh] aspect-square mr-2 relative z-[2] transform translate-y-4 hover:scale-105 transition-transform duration-500"
+          />
+          
+          
+        </div>
+      </div>
+
+      
+    </div>
+    
+  </div>
+  )
+}
+
+export default Zigzag
