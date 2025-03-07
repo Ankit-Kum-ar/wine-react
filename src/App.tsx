21778e27c991245916cb615a909e4c6c8c8bc34a
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-07 11:03:17 +0530

    Add Navbar component and integrate react-router-dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <h1 className="text-center font-bold text-3xl">App</h1>
-      <Button>Click me</Button>
+      <Navbar />
     </>
   )
 }
