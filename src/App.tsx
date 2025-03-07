41ad8b952e9d235b83b4dc7c9adf27d9c5ca28b1
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-07 10:21:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center font-bold text-3xl">App</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
