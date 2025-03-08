dadfdb15b8148ec3fb13dafe98da3b13919ed18e
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-08 17:31:21 +0530

    Fix image paths in Bg and Threeway components for correct background display

--- src/pages/Home/components/Bg.tsx
+++ src/pages/Home/components/Bg.tsx
@@ -2,7 +2,7 @@
 const Bg = () => {
   return (
     <div>
-    <div  className='HeroContent bg-[url(images/webp.png)] bg-cover bg-no-repeat  flex flex-col justify-center h-[100vh] w-full items-center'>
+    <div  className='HeroContent bg-[url(/images/webp.png)] bg-cover bg-no-repeat  flex flex-col justify-center h-[100vh] w-full items-center'>
       
   </div>
   </div>
