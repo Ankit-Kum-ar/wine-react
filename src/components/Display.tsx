88007442d150d3fc25771b38b8e5c5c69f291110
Sahil Rawat <sahilr8587@gmail.com>
2025-03-08 15:12:34 +0530

    Add @types/react-scroll and update component styles for improved layout and readability

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,29 +1,20 @@
-
 interface DisplayProps {
   title: string;
 }
 
 const Display = ({ title }: DisplayProps) => {
   return (
-    <div  className="bg-[#FCF5EA] py-6 px-8 lg:px-16 w-full">
-      <h2 className="text-2xl lg:text-4xl font-sail text-center text-[#1B1C1E] md:mb-10 font-anybody">
+    <div className="bg-[#FCF5EA] py-6 px-4 md:px-8 lg:px-16 w-full">
+      <h2 className="text-xl md:text-2xl lg:text-4xl font-sail text-center text-[#1B1C1E] mb-4 md:mb-10 font-anybody">
         {title}
       </h2>
-      <div className="flex overflow-x-auto space-x-6 py-4 snap-x snap-mandatory ">
-        
-          <div
-            
-            className=" flex justify-center items-center gap-4 p-6 flex-shrink-0 snap-center w-full "
-            
-          >
-            <img src="/images/dom.png" alt="DOM" className="h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
-            <img src="/images/royal salute 21.png" alt="Royal Salute" className="h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
-            <img src="/images/jacob's creek.png" alt="Jacob Creek" className="h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
-            <img src="/images/moet.png" alt="MOET" className="h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
-
-           
-          </div>
-        
+      <div className="flex overflow-x-scroll  py-4 ">
+        <div className="flex items-center gap-4 p-4 md:p-6  w-full">
+          <img src="/images/dom.png" alt="DOM" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
+          <img src="/images/royal salute 21.png" alt="Royal Salute" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
+          <img src="/images/jacob's creek.png" alt="Jacob Creek" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
+          <img src="/images/moet.png" alt="MOET" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
+        </div>
       </div>
     </div>
   )
