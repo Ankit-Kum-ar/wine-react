d1aea2bd97acf8758d1bc40195cfbc26ac904563
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-08 17:09:08 +0530

    Update favicon and document titles for improved branding; set titles for main app and product page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home/Home"
 import Product from "./pages/Product/Product"
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Welcome to WINI"; // Set your desired title here
+  }, []);
   return (
     <>
     <Navbar />
