3d9c6580668348bd7035dbd720b781280c818a6b
Sahil Rawat <sahilr8587@gmail.com>
2025-03-11 17:29:27 +0530

    Replace static image display with CarouselDemo component for enhanced visual presentation in Display component

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,5 @@
+import { title } from "process"
+
 export const carouselImagesArray_01 = [
     "/images/C1.png",
     "/images/C2.png",
@@ -18,6 +20,32 @@ export const carouselImagesArray_02 = [
     "/images/C14.png",
 ]
 
+export const carouselImagesArray_03 = [
+    "/images/C1.png",
+    "/images/C2.png",
+    "/images/C3.png",
+    "/images/C4.png",
+    "/images/C5.png",
+    "/images/C6.png",
+    "/images/C7.png",
+    "/images/C8.png",
+    "/images/C9.png",
+    "/images/C10.png",
+    "/images/C11.png",
+    "/images/C12.png",
+    "/images/C13.png",
+    "/images/C14.png",
+    "/images/P1.png",
+    "/images/P2.png",
+    "/images/P3.png",
+    "/images/P4.png",
+    "/images/P5.png",
+    "/images/P6.png",
+    "/images/SP1.png",
+    "/images/SP2.png",
+
+]
+
 export const carouselImagesContentArray_01 = [
     {
         title: `JACOB'S CREEK`,
@@ -32,6 +60,10 @@ export const carouselImagesContentArray_01 = [
         description: `Grey Goose is a premium vodka brand produced in France. It was originally founded by Sidney Frank before its 2004 sale to Bacardi. The brand is named after the geese that inhabit the region of France where the vodka is produced.`
     },
     {
+        title: `Bombay Sapphire`,
+        description: `Bombay Sapphire is a premium gin that's known for its balanced flavor, versatility, and bright taste. It's made with 10 botanicals, including juniper, coriander, citrus, and peppers`
+    },
+    {
         title : `Tanqueray`,
         description : `The good thing about Tanqueray is its smooth, balanced taste with prominent juniper and herbal notes, making it a classic London Dry gin that is considered a great choice for mixing in cocktails due to its well-rounded flavor profile and four-times distillation process for purity; many also appreciate its iconic bottle design and historical legacy as a well-established brand.`
     },
@@ -43,10 +75,7 @@ export const carouselImagesContentArray_01 = [
         title: `Hendrick’s`,
         description: `The good thing about Hendrick's gin is its unique and balanced flavor profile, characterized by a smooth, floral taste with subtle notes of cucumber and rose, making it a great choice for people who might find traditional, juniper-heavy gins too intense, particularly when mixed in a simple gin and tonic with a cucumber slice as garnish; it's considered a refreshing and approachable option for a wider range of drinkers due to its delicate botanicals.`
     },
-    {
-        title: `Patron`,
-        description: `Patrón Tequila is a brand of tequila products produced in Mexico by the Patrón Spirits Company. Patrón tequila is offered in several different varieties: Gran Patrón Burdeos, Gran Patrón Platinum, Gran Patrón Piedra, Patrón Silver, Patrón Añejo, and Patrón Reposado. It is the best-selling tequila in the world. Patron Tequila is a favorite of many celebrities and is often seen in movies and TV shows.`
-    },
+    
 ]
 
 export const carouselImagesContentArray_02 = [
@@ -55,29 +84,120 @@ export const carouselImagesContentArray_02 = [
         description: `Dom Pérignon is a brand of vintage Champagne produced by the Champagne house Moët & Chandon and serves as that house's prestige champagne. It is named after Dom Pérignon, a Benedictine monk who was an important quality pioneer for Champagne wine but who, contrary to popular myths, did not discover the champagne method for making sparkling wines. Dom Pérignon was the first prestige cuvée, an idea proposed by Englishman Laurence Venn. The first vintage of Dom Pérignon was 1921 and was only released for sale in 1936.`
     },
     {
-        title: `CRISTAL`,
-        description: `Louis Roederer Cristal, the first Prestige Cuvée, was created in 1876 for Alexander II of Russia. This is Champagne at its finest, as inspired today as it has always been, the embodiment of elegance and purity. Cristal is made according to the strictest criteria, based on a drastic process of selection applying to the vintage, the cru, the village, the grapes, and finally the wines. Only Pinot Noir and Chardonnay grapes from the House's 10 most celebrated crus are used in the production of this legendary cuvée.`
+        title: `Jack Daniels`,
+        description: `Jack Daniel's is a Tennessee whiskey known for its smooth, well-balanced flavor and amber hue. It's made from corn, rye, and malted barley, and is distilled in copper stills`
     },
     {
-        title: `Veuve Clicquot`,
-        description: `Veuve Clicquot Ponsardin is a French Champagne house based in Reims, specializing in premium products. Founded in 1772 by Philippe Clicquot-Muiron, Veuve Clicquot played an important role in establishing champagne as a favored drink of haute bourgeoisie and nobility throughout Europe. The 1811 comet vintage of Veuve Clicquot is theorized to have been the first truly "modern" Champagne due to the advancements in the méthode champenoise which Veuve Clicquot pioneered through the technique of remuage.`
+        title: `Camas`,
+        description: ` Camas Merlot is a smooth and approachable red wine with soft tannins and flavors of ripe berries and plum, offering a versatile and easy-drinking option for any occasion.`
     },
     {
-        title : `Krug`,
-        description : `Krug Grande Cuvée is the archetype of Krug's philosophy of craftsmanship and savoir-faire: a blend of more than 120 wines from ten or more different years. Its exceptional finesse is the result of a stay of at least another six years in the cellars. Over twenty years are needed to craft each bottle of Krug Grande Cuvee. Krug Grande Cuvée is the ultimate expression of the Krug style: full-bodied, rich, and intense but always displaying elegance and finesse.`
+        title : `J&B`,
+        description : `J&B Rare is perfect for those who enjoy a classic whisky taste with a modern twist. This smooth Scotch is great on the rocks or neat, and it's also versatile enough to be used in cocktails.`
     },
     {
-        title: `Moet & Chandon`,
-        description: `Moët & Chandon has been the definitive reference for champagne since 1743. Moët Impérial is the House’s iconic champagne. Created in 1869, it embodies Moët & Chandon’s unique style, a style distinguished by its bright fruitiness, its seductive palate, and its elegant maturity. Moët Impérial is created in the heart of Champagne from more than 100 wines (of which 20-30% are reserve wines specially selected to enhance its maturity, complexity, and constancy).`
+        title: `DonJulio`,
+        description: `Don Julio tequila can be enjoyed neat, on the rocks, or in a variety of drinks. It pairs well with certain foods and is often used to make margaritas.`
     },
     {
-        title: `Laurent Perrier`,
-        description: `Laurent-Perrier is a Champagne house founded in 1812. It is the fifth-largest Champagne house in the world and the largest family-owned brand. The house is known for producing a diverse range of Champagne styles, from the classic non-vintage brut to the ultra-premium prestige cuvée Grand Siècle. The house is also known for its pioneering use of stainless steel fermentation and for its focus on Chardonnay, particularly in the ultra-premium range.`
+        title: `Glenfiddich`,
+        description: `Glenfiddich 12 is part of the distillery's flagship collection, a great choice for seasoned sippers or those who are new to whisky, with lots of light and fruity flavours thanks to a high cut point. Aged for 12 years in American and European oak casks, it's also the world's most awarded single malt!`
     },
     {
-        title: `Bollinger`,
-        description: `Bollinger is a Champagne house, a producer of sparkling wines from the Champagne region of France. They produce several labels of Champagne under the Bollinger name, including the vintage Vieilles Vignes Françaises, Grande Année, R.D., and Special Cuvée. Founded in 1829 in Aÿ by Hennequin de Villermont, Paul Renaudin, and Jacques Bollinger, the house continues to be run by members of the Bollinger family.` 
+        title: `Royal Salute`,
+        description: `Royal Salute The Signature Blend is a spectacular whisky. On the nose, sweet pears and citrus fruits are balanced with the fragrance of autumn flowers. An elegant blend of vanilla and dry oak is crowned with subtle notes of sherry and just a wisp of smoke.` 
     },
+]
+
+export const carouselImagesContentArray_03 = [
+    {
+        title: `JACOB'S CREEK`,
+        description: ``
+    },
+    {
+        title: `BELUGA`,
+        description: ``
+    },
+    {
+        title: `GREY GOOSE`,
+        description: ``
+    },
+    {
+        title : `Bomabay Sapphire`,
+        description : ``
+    },
+    {
+        title : `Tanqueray`,
+        description : ``
+    },
+    {
+        title: `Monkey 47`,
+        description: ``
+    },
+    {
+        title: `Hendrick's`,
+        description: ``
+    },
+    {
+        title: `DOM Perignon`,
+        description: ``
+    },
+    {
+        title: `Jack Daniels`,
+        description: ``
+    },
+    {
+        title: `Camas`,
+        description: ``
+    },
+    {
+        title : `J&B`,
+        description : ``
+    },
+    {
+        title: `DonJulio`,
+        description: ``
+    },
+    {
+        title: `Glenfiddich`,
+        description: ``
+    },
+    {
+        title: `Royal Salute`,
+        description: `` 
+    },
+    {
+        title: `Blue Label`,
+        description: ``
+    },
+    {
+        title: `Red Label`,
+        description: ``
+    },
+    {
+        title: `The Glenlivet 12`,
+        description: ``
+    },
+    {
+        title: `The Glenlivet `,
+        description: ``
+    },
+    {
+        title: `The Singleton`,
+        description: ``
+    },
+    {
+        title: `Jameson`,
+        description: ``
+    },
+    {
+        title: `Glenfiddich`,
+        description: ``
+    },
+    {
+        title: `Glenfiddich Vat 03`,
+        description: ``
+    }
 ]
 
 export const displayProductContent = {
@@ -93,14 +213,15 @@ export const displayProductContent = {
     title_4 : `The Glenlivet 12`,
     title_5 : `The Singleton`,
     title_6 : `Jameson`,
-    desc_a_1: `Red Rebel Ice Pick is a melee weapon that provides access to additional extraction points on Reserve and Lighthouse maps, but also you can chop your enemies with that! Didn't find the ice pick yet? You ca buy it from our professional players!`,
-    desc_a_2: `Red Rebel Ice Pick is a melee weapon that provides access to additional extraction points on Reserve and Lighthouse maps, but also you can chop your enemies with that! Didn't find the ice pick yet? You ca buy it from our professional players!`,
-    desc_b_2: `Blue Rebel Ice Pick is a melee weapon that provides access to additional extraction points on Reserve and Lighthouse maps, but also you can chop your enemies with that! Didn't find the ice pick yet? You ca buy it from our professional players!`,
-    desc_b_1: `Blue Rebel Ice Pick is a melee weapon that provides access to additional extraction points on Reserve and Lighthouse maps, but also you can chop your enemies with that! Didn't find the ice pick yet? You ca buy it from our professional players!`,
-    desc_c_1: `He whisky comes in at an approachable 40% ABV and is matured in both European and American oak casks (not particularly telling).`,
-    desc_c_2: `The Glenlivet 12-Year is the epitome of an acceptable, entry-level Speyside single malt scotch. Like many of its peers, this makes it easier to enjoy from a casual, entry-level standpoint.`,
-    desc_d_1: `It is the oldest legal distillery in the Highlands of Scotland.`,
-    desc_d_2: `It was founded in 1824 and has operated almost continuously since. The distillery remained open throughout the Great Depression and its only closure came during World War II.`,
+    desc_a_1: `Red Label: A non-age-stated blend. It has been the best selling Scotch whisky in the world since 1945. It is primarily used for making mixed drinks. Black Label: Aged 12 years, it is one of the world's best-selling Scotch whiskies.`,
+    desc_a_2: ``,
+    desc_b_2: `It is made up of the finest old-aged malt and grain whiskies in Diageo's extensive portfolio. Casks from Cardhu, Clynelish, Benrinnes and more are hand-selected for this blend to ensure the highest quality.`,
+    desc_b_1: `Blue Label is the most prestigious whisky in Johnnie Walker's core range.`,
+    desc_c_1: `Smooth, balanced, and rich in character, The Glenlivet 12 is the gold standard of Speyside single malts. With its signature notes of ripe orchard fruits, honey, and a gentle oak finish, this whisky embodies the perfect harmony of tradition and refinement.`,
+    desc_c_2: ``,
+    desc_d_1: `The Glenlivet is more than just a whisky—it’s a legacy. Since 1824, we have set the standard for smooth, rich, and complex single malts, cherished by whisky lovers worldwide. Every sip tells a story of tradition, innovation, and an unwavering commitment to quality.
+Raise a glass to timeless craftsmanship.`,
+    desc_d_2: ``,
     desc_e_1: `The Singleton is celebrated for its exceptionally smooth texture and profound flavours—a testament to the care given at each stage of its creation. Originating from the distilleries of Glen Ord, Glendullan, and Dufftown, this award-winning Single Malt Scotch Whisky is a masterpiece of patience and expertise.`,
     desc_f_1: `Jameson Irish Whiskey is a blended Irish whiskey. What’s that we hear you say. Well first we take the best of pot still and fine grain whiskeys. Then we triple distil them - not because we have to because we want to as it gives it its signature smoothness. Finally, we age them in oak casks for a minimum of 4 years.`,
 }
