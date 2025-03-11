d238569b40631084916d10499d77731e665e9ca5
Ankit-0009 <ankitcode2511@gmail.com>
2025-03-11 13:00:58 +0530

    Adjust title margin and padding in Display component for improved layout consistency

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,10 +5,10 @@ interface DisplayProps {
 const Display = ({ title }: DisplayProps) => {
   return (
     <div className="bg-[#FCF5EA] py-6 px-4 md:px-8 lg:px-16 w-full">
-      <h2 className="text-xl md:text-2xl lg:text-4xl font-sail text-center text-[#1B1C1E] mb-4 ">
+      <h2 className="text-xl md:text-2xl lg:text-4xl font-sail text-center text-[#1B1C1E] my-4 ">
         {title}
       </h2>
-      <div className="flex overflow-x-scroll  py-4 ">
+      <div className="flex overflow-x-scroll pb-4 ">
         <div className="flex items-center gap-4 p-4 md:p-6  w-full">
           <img src="/images/dom.png" alt="DOM" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
           <img src="/images/royal salute 21.png" alt="Royal Salute" className="h-40 md:h-[60vh] transform translate-y-4 hover:scale-105 transition-transform duration-500" />
